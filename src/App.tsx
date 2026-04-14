60a79d97697d208a5eb8d4cdcd448c91c3a81a15
Sal <nguyennguyenthan.SH@gmail.com>
2026-04-14 23:53:22 +0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -186,7 +186,7 @@ const INIT_NOTES: Note[] = [
     {name:'T2',completed:5,planned:8},{name:'T3',completed:7,planned:7},{name:'T4',completed:4,planned:10},
     {name:'T5',completed:8,planned:9},{name:'T6',completed:6,planned:6},{name:'T7',completed:3,planned:4},{name:'CN',completed:2,planned:2},
   ];
-  const weeklyData=[
+  const giweeklyData=[
     {name:'Tuần 1',created:20,completed:15},{name:'Tuần 2',created:25,completed:22},
     {name:'Tuần 3',created:18,completed:20},{name:'Tuần 4',created:30,completed:25},
   ];
